2e1bafead7e74c7ae1c00b07ca79bb0d63657afa
pratikkhedekar42 <31773805+pratikkhedekar42@users.noreply.github.com>
2021-02-08 14:48:11 +0530

    Add files via upload

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+
+region             = "us-central1"
+gcp_project_id     = "grounded-garage-304206"
+name               = "pratik"
+subnet_cidr        = "10.20.0.0/24"
+my_inst_type       = "f1-micro"
---
+++ Terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type = string
+}
+variable "gcp_project_id" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "subnet_cidr" {
+
+}
+
+variable "my_inst_type"{
+  type = string
+  description = "my instance type"
+}
